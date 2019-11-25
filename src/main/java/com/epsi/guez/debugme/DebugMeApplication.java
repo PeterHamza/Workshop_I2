c1bfb95b7be8b936ac6dd3044bf4ab3a913d551b
Jordan Noel <jordannoel1@live.fr>
2019-11-25 16:08:19 +0100

    Premier commit

---
+++ src/main/java/com/epsi/guez/debugme/DebugMeApplication.java
@@ -0,0 +1,11 @@
+package com.epsi.guez.debugme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebugMeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DebugMeApplication.class, args);
+    }
+}
